f31aeefb09bed589cad04852081684ceeb3e2553
serii <seriiburduja@gmail.com>
2024-06-06 08:52:22 +0300

    feat: content changes

--- js/content.js
+++ js/content.js
@@ -1,20 +1,19 @@
-let intervalId
-
+let intervalId;
 
 const addClock = () => {
-console.log('addClock')
-    let overlay = document.querySelector('.easyIt-ext')
-    if (!overlay) {
-        overlay = document.createElement('div')
-        overlay.setAttribute('class', 'easyIt-ext')
-    }
+  console.log("addClock");
+  let overlay = document.querySelector(".easyIt-ext");
+  if (!overlay) {
+    overlay = document.createElement("div");
+    overlay.setAttribute("class", "easyIt-ext");
+  }
 
-    overlay.innerHTML = `
+  overlay.innerHTML = `
          <div class="easyIt-data"></div>
-    `
+    `;
 
-    const style = document.createElement('style')
-    style.textContent = `
+  const style = document.createElement("style");
+  style.textContent = `
        .easyIt-ext{
         position: fixed;
         top: 0;
@@ -35,49 +34,44 @@ console.log('addClock')
           color: #fff;
           padding: 50px;
        }
-    `
-    const body = document.querySelector('body')
-    body.appendChild(overlay)
-    body.appendChild(style)
+    `;
+  const body = document.querySelector("body");
+  body.appendChild(overlay);
+  body.appendChild(style);
 
-    intervalId = setInterval(() => {
-        const data = document.querySelector('.easyIt-data')
-        if (data) {
-            const d = new Date()
-            const hours = `${d.getHours()}`
-            const mins = `${d.getMinutes()}`
-            const secs = `${d.getSeconds()}`
+  intervalId = setInterval(() => {
+    const data = document.querySelector(".easyIt-data");
+    if (data) {
+      const d = new Date();
+      const hours = `${d.getHours()}`;
+      const mins = `${d.getMinutes()}`;
+      const secs = `${d.getSeconds()}`;
 
-            data.textContent = `${hours.padStart(2, '0')}:${mins.padStart(2, '0')}:${secs.padStart(2, '0')}`
-        }
-    }, 1000)
-}
+      data.textContent = `${hours.padStart(2, "0")}:${mins.padStart(2, "0")}:${secs.padStart(2, "0")}`;
+    }
+  }, 1000);
+};
 
 const removeClock = () => {
-    clearInterval(intervalId)
-    const content = document.querySelector('.easyIt-ext')
-    if (content) {
-        content.parentNode.removeChild(content)
-    }
-}
+  clearInterval(intervalId);
+  const content = document.querySelector(".easyIt-ext");
+  if (content) {
+    content.parentNode.removeChild(content);
+  }
+};
 
-chrome.storage.sync.get(['showClock'], (result) => {
-    if (result.showClock) {
-        addClock()
-    }
+chrome.storage.sync.get(["showClock"], (result) => {
+  if (result.showClock) {
+    addClock();
+  }
 });
 
 chrome.storage.onChanged.addListener((changes, namespace) => {
-    if (changes?.showClock) {
-        if (changes.showClock.newValue) {
-            addClock()
-        } else {
-            removeClock()
-        }
+  if (changes?.showClock) {
+    if (changes.showClock.newValue) {
+      addClock();
+    } else {
+      removeClock();
     }
+  }
 });
-
-
-
-
-
